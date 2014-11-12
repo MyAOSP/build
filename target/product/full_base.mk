a934e49ea11a8d9107720ecb46f8591e59bd9243
MrBaNkS <banksmi09@gmail.com>
2014-11-12 04:16:14 -0600

    Change default tones

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -29,8 +29,8 @@ PRODUCT_PACKAGES += \
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
+    ro.config.ringtone=Titania.ogg \
+    ro.config.notification_sound=Europa.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
